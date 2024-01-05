42bd021cd6e04de2a7c205e5642b133b6c93ee07
ErdoganSeref <erdogan872@gmail.com>
2024-01-05 01:19:03 +0100

    Extract conversion from game to move indexes into class

--- src/main/java/game/Encoder.java
+++ src/main/java/game/Encoder.java
@@ -5,8 +5,6 @@ import org.lichess.compression.BitWriter;
 import org.lichess.compression.game.codec.Rans;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Encoder {
     private final Rans rans = new Rans();
@@ -18,90 +16,9 @@ public class Encoder {
         }
     };
 
-    private static Pattern SAN_PATTERN = Pattern.compile(
-        "([NBKRQ])?([a-h])?([1-8])?x?([a-h][1-8])(?:=([NBRQK]))?[\\+#]?");
-
-    private static Role charToRole(char c) {
-        switch (c) {
-            case 'N': return Role.KNIGHT;
-            case 'B': return Role.BISHOP;
-            case 'R': return Role.ROOK;
-            case 'Q': return Role.QUEEN;
-            case 'K': return Role.KING;
-            default: throw new IllegalArgumentException();
-        }
-    }
-
-    private static int[] getMoveIndexes(String[] pgnMoves) {
-        Board board = new Board();
-        MoveList legals = moveList.get();
-
-        int[] moveIndexes = new int[pgnMoves.length];
-
-        for (int ply = 0; ply < pgnMoves.length; ply++) {
-            String pgnMove = pgnMoves[ply];
-            Lan current = parseSan(pgnMove, board);
-
-            // Find index in legal moves.
-            board.legalMoves(legals);
-            legals.sort();
-
-            boolean foundMatch = false;
-            int size = legals.size();
-
-            for (int i = 0; i < size; i++) {
-                Move legal = legals.get(i);
-                if (legal.role == current.role && legal.to == current.to && legal.promotion == current.promotion && Bitboard.contains(current.from, legal.from)) {
-                    if (!foundMatch) {
-                        // Save and play.
-                        moveIndexes[ply] = i;
-                        board.play(legal);
-                        foundMatch = true;
-                    } else return null;
-                }
-            }
-
-            if (!foundMatch) return null;
-        }
-        return moveIndexes;
-    }
-
-    private static Lan parseSan(String pgnMove, Board board) {
-        Role role;
-        Role promotion = null;
-        long from = Bitboard.ALL;
-        int to;
-
-        if (pgnMove.startsWith("O-O-O")) {
-            role = Role.KING;
-            from = board.kings;
-            to = Bitboard.lsb(board.rooks & Bitboard.RANKS[board.turn ? 0 : 7]);
-        } else if (pgnMove.startsWith("O-O")) {
-            role = Role.KING;
-            from = board.kings;
-            to = Bitboard.msb(board.rooks & Bitboard.RANKS[board.turn ? 0 : 7]);
-        } else {
-            Matcher matcher = SAN_PATTERN.matcher(pgnMove);
-            if (!matcher.matches()) return null;
-
-            String roleStr = matcher.group(1);
-            role = roleStr == null ? Role.PAWN : charToRole(roleStr.charAt(0));
-
-            if (matcher.group(2) != null) from &= Bitboard.FILES[matcher.group(2).charAt(0) - 'a'];
-            if (matcher.group(3) != null) from &= Bitboard.RANKS[matcher.group(3).charAt(0) - '1'];
-
-            to = Square.square(matcher.group(4).charAt(0) - 'a', matcher.group(4).charAt(1) - '1');
-
-            if (matcher.group(5) != null) {
-                promotion = charToRole(matcher.group(5).charAt(0));
-            }
-        }
-        return new Lan(role, promotion, from, to);
-    }
-
     public EncodeResult encode(String[] pgnMoves) {
         BitWriter writer = new BitWriter();
-        int[] moveIndexes = getMoveIndexes(pgnMoves);
+        int[] moveIndexes = GameToMoveIndexesConverter.convert(pgnMoves);
         if (moveIndexes == null) {
             return null;
         }
@@ -191,20 +108,6 @@ public class Encoder {
             this.halfMoveClock = halfMoveClock;
             this.positionHashes = positionHashes;
             this.lastUci = lastUci;
-        }
-    }
-
-    public static class Lan {
-        public final Role role;
-        public final Role promotion;
-        public long from;
-        public int to;
-
-        public Lan(Role role, Role promotion, long from, int to) {
-            this.role = role;
-            this.promotion = promotion;
-            this.from = from;
-            this.to = to;
         }
     }
 
